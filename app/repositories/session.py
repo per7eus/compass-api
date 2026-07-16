da0525698ff6d28f414b31e71586275533fa55cc
Tor344 <per7euse@gmail.com>
2026-07-16 21:08:25 +0300

    feat: add create session by id

--- app/repositories/session.py
+++ app/repositories/session.py
@@ -1,7 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
-from  ..database.models import Session
+from fastapi import HTTPException, status
+
+from  ..database.models import Session, User
+
 
 class SessionRepository:
     def __init__(self, session: AsyncSession):
@@ -36,4 +39,22 @@ class SessionRepository:
 
         session.result = result
         await self.session.commit()
+
+    async def create_by_tid(self, tid1: int,answer1:str,test_id: int):
+        try:
+            user_id = await self.session.scalar(select(User.id).where(User.tid == tid1))
+            if not user_id:
+                return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+            return await  self.create(user_id, answer1, test_id)
+        except Exception as e:
+            HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+    async def set_id2_answer2_result_by_tid(self,session_id: int,tid2: int, answer2:str, result:str):
+        try:
+            id2 = await self.session.scalar(select(User.id).where(User.tid == tid2))
+            if not id2:
+                return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+            return await  self.set_id2_answer2_result(session_id=session_id,id2=id2, answer2=answer2, result=result)
+        except Exception as e:
+            HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
--- app/routers/session.py
+++ app/routers/session.py
@@ -3,7 +3,7 @@ from fastapi.params import Depends
 
 
 from ..services.session import SessionService
-from ..schemas.session import CreateSessionSchema, JoinSessionSchema
+from ..schemas.session import CreateSessionSchema, JoinSessionSchema, CreateSessionByTidSchema, JoinSessionByTidSchema
 
 from ..dependencies import get_session_service
 
@@ -26,4 +26,15 @@ async def create(session: CreateSessionSchema, session_service: SessionService =
 @session_router.post("/{session_id}/join")
 async def join(session_id, session_join: JoinSessionSchema,session_service: SessionService = Depends(get_session_service)):
     result = await session_service.join_session(session_id, session_join.model_dump())
+    return {"result": result}
+
+
+@session_router.post("/create/by_tid")
+async def create_by_tid(session: CreateSessionByTidSchema, session_service: SessionService = Depends(get_session_service)):
+    id = await session_service.create_by_tid(session.model_dump())
+    return {"session_id": id}
+
+@session_router.post("/{session_id}/join/by_id")
+async def join(session_id, session_join: JoinSessionByTidSchema,session_service: SessionService = Depends(get_session_service)):
+    result = await session_service.join_session_by_tid(session_id, session_join.model_dump())
     return {"result": result}
